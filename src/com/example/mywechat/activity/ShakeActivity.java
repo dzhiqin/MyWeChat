8b2482c03fe27a9201083b40d6b2c9843fe55057
dzhiqin <dzhiqin@126.com>
2016-08-24 07:42:56 +0800

    create shake_activity

--- src/com/example/mywechat/activity/ShakeActivity.java
+++ src/com/example/mywechat/activity/ShakeActivity.java
@@ -1,5 +1,42 @@
 package com.example.mywechat.activity;
 
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.widget.RelativeLayout;
+
+import com.example.mywechat.R;
+import com.example.mywechat.listener.ShakeListener;
+import com.example.mywechat.listener.ShakeListener.OnShakeListener;
+
 public class ShakeActivity extends BaseActivity{
 
+	ShakeListener mShakeListener=null;
+	/**
+	 * 震动器，手机震动使用
+	 */
+	Vibrator mVibrator;
+	private RelativeLayout mImgUp;
+	private RelativeLayout mImgDown;
+	private RelativeLayout mTitle;
+	@Override 
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.shake_activity);
+		mVibrator=(Vibrator)getApplication().getSystemService(VIBRATOR_SERVICE);
+		mImgUp=(RelativeLayout)findViewById(R.id.shakeImgUp);
+		mImgDown=(RelativeLayout)findViewById(R.id.shakeImgDown);
+		mTitle=(RelativeLayout)findViewById(R.id.shake_title_bar);
+	
+		//设置晃动监听
+		mShakeListener=new ShakeListener(this);
+		mShakeListener.setOnShakeListener(new OnShakeListener(){
+	
+			@Override
+			public void onShake() {
+				// TODO 自动生成的方法存根
+				
+			}
+			
+		});
+	}
 }
